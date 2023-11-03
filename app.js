f4f3ccd04e4bfac7ecc9ee38735f1c10dc75bb7d
dnk92 <aaze92@gmail.com.com>
2023-11-02 21:27:58 -0600

    añado datos

--- app.js
+++ app.js
@@ -1,48 +1,11 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
-
-// Define a schema for a user
-const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    age: Number
-});
-
-// Define a model for the user schema
-const User = mongoose.model('User', userSchema);
-
-// Define a route to get all users
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find();
-        res.json(users);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-});
-
-// Define a route to create a new user
-app.post('/users', async (req, res) => {
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        age: req.body.age
-    });
-
-    try {
-        const newUser = await user.save();
-        res.status(201).json(newUser);
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
-});
-
-// Start the server
-app.listen(3000, () => console.log('Server started'));
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})

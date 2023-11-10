1408c2356296e16cb0785d3a7b63d304aaf6bd3b
Arturo Zepeda(server) <aaze92@gmail.com>
2023-11-10 05:23:02 +0000

    correcciones para leer .ENV

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const express = require('express')
 require('dotenv').config();
+//require('dotenv').config({path:"./.ENV"});
+console.log(process.env);
 const app = express();
 const port = 3000;
 console.log(process.env.MONGODB_URI);

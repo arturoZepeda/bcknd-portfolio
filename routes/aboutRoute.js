68583e84a2cfa4516a644972517f4e94d13bb5f9
Arturo Zepeda <aaze92@gmail.com>
2023-11-15 20:51:02 -0600

    correcciones de typos

--- routes/aboutRoute.js
+++ routes/aboutRoute.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const aboutController = require('../controllers/aboutController');
 const aboutCont = new aboutController();
-router.route('/:aboutId')
+
+router.route('/aboutId')
   .get(aboutCont.getAbout)
   .post(aboutCont.postAbout)
   .put(aboutCont.putAbout)
-  .delete(aboutCont.deleteAbout)
+  .delete(aboutCont.deleteAbout);
 
-  module.exports = router;
+module.exports = router;

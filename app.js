145a46c0e7b61ae2855d4310923d45e35db90202
Arturo Zepeda(server) <aaze92@gmail.com>
2023-11-18 07:00:25 +0000

    instalo y configuro CORS para poder hacer request desde app de vue

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 //require('dotenv').config({path:"./.ENV"});
 const app = express();
 const port = 3000;
-app.all('*',function (req,res) {
+app.use(cors());
+/*app.all('*',function (req,res) {
  res.header("Access-Control-Allow-Origin", "*");
  res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-});
+});*/
 console.log(process.env.MONGODB_URI);
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Produccion');
 app.use(express.json());
